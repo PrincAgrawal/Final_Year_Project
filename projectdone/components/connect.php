6a636ddabf0e5bb2d7729f48fe71cf701f2af7cf
Prince Agrawal <princeagrawal783@gmail.com>
2025-04-02 23:18:35 +0530

    Initial commit

---
+++ projectdone/components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=localhost;dbname=circuitfix_shop_db';
+$user_name = 'root';
+$user_password = 'Prince@2004@';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
